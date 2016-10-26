424db2f42349f2344539f2696efc097f87f8c7a2
sotomayormatias <matisoto_77@hotmail.com>
2016-10-25 23:43:53 -0300

    TP3

---
+++ Sotomayor.Matias.2C.TP3/EntidadesAbstractas/Persona.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesAbstractas
+{
+    public abstract class Persona
+    {
+        public enum ENacionalidad { Argentino, Extranjero };
+
+        #region Atributos y Propiedades
+        private string _apellido;
+
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+
+        private int _dni;
+
+        public int DNI
+        {
+            get { return _dni; }
+            set { _dni = value; }
+        }
+
+        private ENacionalidad _nacionalidad;
+
+        public ENacionalidad Nacionalidad
+        {
+            get { return _nacionalidad; }
+            set { _nacionalidad = value; }
+        }
+
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        } 
+        #endregion
+        
+    }
+}
