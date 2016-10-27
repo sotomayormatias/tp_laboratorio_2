d62911a0e3295dd08f958350baa843912c6ec98f
sotomayormatias <matisoto_77@hotmail.com>
2016-10-26 22:03:46 -0300

    Se agrega codigo de clase Alumno

---
+++ Sotomayor.Matias.2C.TP3/EntidadesInstanciables/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesAbstractas;
+
+namespace EntidadesInstanciables
+{
+    public sealed class Alumno : PersonaGimnasio
+    {
+        public enum EEstadoCuenta { MesPrueba, Deudor, AlDia };
+
+        private Gimnasio.EClases _claseQueToma;
+        private EEstadoCuenta _estadoCuenta;
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Gimnasio.EClases claseQueToma)
+            : base(id, nombre, apellido, dni, nacionalidad)
+        {
+            this._claseQueToma = claseQueToma;
+        }
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, Gimnasio.EClases claseQueToma, EEstadoCuenta estadoCuenta)
+            : this(id, nombre, apellido, dni, nacionalidad, claseQueToma)
+        {
+            this._estadoCuenta = estadoCuenta;
+        }
+    }
+}
---
+++ Sotomayor.Matias.2C.TP3/EntidadesInstanciables/Gimnasio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesInstanciables
+{
+    public class Gimnasio
+    {
+        public enum EClases { Crossfit, Natacion, Pilates, Yoga };
+    }
+}
