8030b4fe42200c4998e8c887ff3857ae1ece89a0
sotomayormatias <matisoto_77@hotmail.com>
2016-10-29 10:52:57 -0300

    clase jornada

---
+++ Sotomayor.Matias.2C.TP3/EntidadesInstanciables/Jornada.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesInstanciables
+{
+    public class Jornada
+    {
+        private List<Alumno> _alumnos;
+        private Gimnasio.EClases _clase;
+        private Instructor _instructor;
+
+        public Jornada() 
+        {
+            this._alumnos = new List<Alumno>();
+        }
+
+        public Jornada(Gimnasio.EClases clase, Instructor instructor)
+            : this()
+        {
+            this._clase = clase;
+            this._instructor = instructor;
+        }
+    }
+}
